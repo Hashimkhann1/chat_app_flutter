86ca5872101ce806d8060630803fdcd08939cfb7
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-09-06 21:32:15 +0500

    fluttter chat app Project setup

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        
+      ),
+    );
+  }
+}
