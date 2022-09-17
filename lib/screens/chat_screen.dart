9b6235bef9e651fd5886b924799bdd0be047de3f
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-09-17 22:41:29 +0500

    chat screen UI is updated

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,10 +20,57 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        actions: <Widget>[
+          IconButton(onPressed: () {
+            _auth.signOut();
+            Navigator.pop(context);
+          },
+              icon: Icon(Icons.close))
+        ],
+        title: Text('⚡️Chat'),
+      ),
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text('Chat screen'),
+            Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.black
+                      ),
+                      onChanged: (value){
+                        // print(value);
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Enter your message',
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 28.0),
+
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: (){},
+                      child: Text('Send',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700
+                        ),
+                  ))
+                ],
+              ),
+            )
           ],
         ),
       ),
